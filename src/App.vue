c35e329328d1dced88061749f960beceeb23c6b7
Nicolas-Dolan <nicolas.dolan@ymail.com>
2025-05-18 21:55:02 +0100

    added dark mode toggle

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,12 @@
 import SeoForm from './components/SeoForm.vue'
 import ResultsPanel from './components/ResultsPanel.vue'
 import SearchHistoryTable from './components/SearchHistoryTable.vue'
+import DarkModeToggle from './components/DarkModeToggle.vue'
 </script>
 
 <template>
   <div class="wrapper">
+    <DarkModeToggle />
     <h1>SEO Keyword Ranking Tool</h1>
     <SeoForm />
     <ResultsPanel />
---
+++ src/components/DarkModeToggle.vue
@@ -0,0 +1,17 @@
+<template>
+  <Button
+    :label="isDark ? 'Dark' : 'Light'"
+    :icon="isDark ? 'pi pi-moon' : 'pi pi-sun'"
+    @click="toggleDarkMode()"
+  />
+</template>
+
+<script setup lang="ts">
+import Button from 'primevue/button'
+import { useDark, useToggle } from '@vueuse/core'
+
+const isDark = useDark()
+const toggleDarkMode = useToggle(isDark)
+</script>
+
+<style scoped></style>
